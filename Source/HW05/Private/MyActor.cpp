e002ac07a374a1ecee9d8de6c8d4e949fbf4fbea
검성\twili <jyjh4837@gmail.com>
2026-03-26 23:46:37 +0900

    HW05

---
+++ Source/HW05/Private/MyActor.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void TriggerEventWithProbability(float Probability)
+{
+    int32 RandomValue = FMath::RandRange(1, 100); // 1 ~ 100의 난수 생성
+    if (RandomValue <= Probability)
+    {
+        UE_LOG(LogTemp, Log, TEXT("Event Triggered!"));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Log, TEXT("Event Not Triggered"));
+    }
+}
